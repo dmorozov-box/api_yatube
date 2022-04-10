9a98dd72201dfc47d3e1f0423c3a00bdc96db4f1
Dmitry Morozov <dmorozov.box@yandex.ru>
2022-04-10 20:20:38 +0300

    Версия 1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,7 +8,7 @@ from .views import GroupViewSet, PostViewSet, CommentViewSet
 router = SimpleRouter()
 router.register('groups', GroupViewSet)
 router.register('posts', PostViewSet)
-router.register('posts/(?P<post_id>\d+)/comments', CommentViewSet)
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
